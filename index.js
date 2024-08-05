269c4cc7a46ff6abfabe0bdab791ee923a75a071
YASHrnw <140162246+YASHrnw@users.noreply.github.com>
2024-08-05 11:16:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+
+
+const http = require('http');
+
+const dataControl = (req, resp) => {
+    resp.write("<h1>Hello, this is Mufij</h1>");
+    resp.end();
+}
+
+http.createServer(dataControl).listen(4501, () => {
+    console.log("Server is listening on port 4501");
+});
