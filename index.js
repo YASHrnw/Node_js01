b3a50e2f980f7346f425cb20e0498c080b6a8a59
YASHrnw <f347yash@gmail.com>
2024-08-05 19:20:31 +0530

    add

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello");
+// console.log("hello");
+
+
+const http = require('http');
+
+const dataControl = (req, resp) => {
+    resp.write("<h1>yash</h1>");
+    resp.end();
+}
+
+http.createServer(dataControl).listen(4501, () => {
+    console.log("Server port 4501");
+});
+
+
+
+// var http = require('http');
+// var fs = require('fs');
+// http.createServer(function (req, res) {
+//     //Open a file on the server and return its content:
+//     fs.readFile('demofile1.html', function (err, data) {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);
+
