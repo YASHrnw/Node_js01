8295fb1d6c25dd9c29a1546740f2237433f91588
YASHrnw <f347yash@gmail.com>
2024-08-08 09:15:08 +0530

    add

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // console.log("hello");
 
 
-const http = require('http');
+// const http = require('http');
 
-const dataControl = (req, resp) => {
-    resp.write("<h1>yash</h1>");
-    resp.end();
-}
+// const dataControl = (req, resp) => {
+//     resp.write("<h1>yash</h1>");
+//     resp.end();
+// }
 
-http.createServer(dataControl).listen(4501, () => {
-    console.log("Server port 4501");
-});
+// http.createServer(dataControl).listen(4501, () => {
+//     console.log("Server port 4501");
+// });
 
 
 
--- second.js
+++ second.js
@@ -1,16 +1,26 @@
 const express = require('express');
-
 const app = express();
 
+app.set('view engine', 'ejs')
+
 // root route
-app.get('/', (req, res) => {
-  res.send("<h1>yash panchal</h1>");
+// app.get('/', (req, res) => {
+//   res.send("<h1>yash panchal</h1>");
+// });
+// app.get('/Profile', (req, res) => {
+//   res.send("<h1>yash@2212</h1>");
+// });
+
+
+app.get('/profile', (__, res) => {
+  const user = {
+    name: "YASH",
+    age: 22,
+    skills:['php','js','c++']
+  }
+  res.render('profile',{user});
 });
+app.listen(4200);
 
 
-app.get('/panchal', (req, res) => {
-    res.send("<h1>yash@2212</h1>");
-  });
-  
 
-app.listen(420);
